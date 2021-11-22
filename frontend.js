4cad749ba04d152f916cac71cd5175e2cd2a2849
vladislavbondarenko <bondarencko40@gmail.com>
2021-11-22 16:32:21 +0300

    New rule

--- frontend.js
+++ frontend.js
@@ -12,6 +12,7 @@ module.exports = {
     },
 
     rules: {
+        "@typescript-eslint/ban-ts-ignore": "off",
         'jsx-a11y/anchor-is-valid' : 'off',
         'jsx-a11y/click-events-have-key-events':   'off',
         'jsx-a11y/no-static-element-interactions': 'off',
