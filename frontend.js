9657ae8bbf3137e2b08465fb6b0e830c749c3060
vladislavbondarenko <bondarencko40@gmail.com>
2021-11-22 15:42:09 +0300

    Disable anchor error

--- frontend.js
+++ frontend.js
@@ -12,6 +12,7 @@ module.exports = {
     },
 
     rules: {
+        'jsx-a11y/anchor-is-valid' : 'off',
         'jsx-a11y/click-events-have-key-events':   'off',
         'jsx-a11y/no-static-element-interactions': 'off',
         'react/destructuring-assignment':          'off',
